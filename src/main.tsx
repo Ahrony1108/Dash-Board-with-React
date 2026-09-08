c24323d80216e9e7d286f300339a6217bed674db
Rony <it12@daffodilvarsity.edu.bd>
2026-09-08 19:30:37 +0600

    test react rendering

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 style={{ color: 'red', padding: '50px' }}>
-      React is working!
-    </h1>
-  </React.StrictMode>
+    <h1 style={{ color: 'red', padding: '50px' }}>React is working!</h1>
+  </React.StrictMode>,
 );
