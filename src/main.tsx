218cacba646dae6e139d38279d861c3bf96fc9cc
Rony <it12@daffodilvarsity.edu.bd>
2026-09-08 19:25:49 +0600

    test react rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import BreakpointsProvider from 'providers/BreakpointsProvider';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from 'routes/router';
-
-import { theme } from 'theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BreakpointsProvider>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </BreakpointsProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
+    <h1 style={{ color: 'red', padding: '50px' }}>
+      React is working!
+    </h1>
+  </React.StrictMode>
 );
